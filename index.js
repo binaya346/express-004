6aa39fbdefa78609c8627c56ae9d75fa73699ec8
binayarijal-qbit <binaya.rijal@qbits.ltd>
2026-01-27 18:34:39 +0545

    connection with mongodb

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 import express from 'express'
+import mongoose from 'mongoose';
 const app = express()
+
+try {
+    await mongoose.connect('mongodb://127.0.0.1:27017/antbyte')
+    app.listen(8080, () => {
+        console.log('Server is running on http://localhost:8080')
+    })
+    console.log('Successfully connected with database')
+}catch (e) {
+    console.log("failed to connect")
+}
 
 // API GET, POST, DELETE, PUT
 app.get('/', (req, res) => {
@@ -15,7 +26,3 @@ app.get("/api", (req, res) => {
 app.get("/contact", (req, res) => {
     res.json({ "mesage": "hello from contact page" })
 });
-
-app.listen(8080, () => {
-    console.log('Server is running on http://localhost:8080')
-})
